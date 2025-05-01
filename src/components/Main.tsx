24957a3fd4ee5ff6549a7a70d19bbb43f96fcc2e
Zubaer Milky <milkyzubaer@gmail.com>
2025-05-01 18:37:02 -0400

    Update Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,17 +2,22 @@ import React from "react";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import "../assets/styles/Main.scss";
+import bgDark from "../assets/images/bg-dark.png";
 
 function Main() {
   return (
     <div className="container">
       <div className="about-section">
+        {/* Decorative dark image as a separate element */}
+        <img src={bgDark} alt="" className="bg-overlay-image" />
+
         <div className="image-wrapper">
           <img
             src="https://raw.githubusercontent.com/zubaer012/misc-assets/profile/headshot.png"
             alt="Avatar"
           />
         </div>
+
         <div className="content">
           <div className="social_icons">
             <a
@@ -30,6 +35,7 @@ function Main() {
               <LinkedInIcon />
             </a>
           </div>
+
           <h1>Zubaer Milky</h1>
           <p>Software Developer</p>
 
